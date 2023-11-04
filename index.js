cd56451e5e9a99f71d00d4266f5ee8acb875b631
Jefté Medeiros <jefte.medeiros8@gmail.com>
2023-11-04 13:54:03 -0500

    casca de bala added

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+  handleToggleCat();
+});
+
+const catSwitch = document.querySelector("#switch")
+const catSwitchBall = document.querySelector("#switch-ball")
+const catPic = document.querySelector("#cat-pic")
+
+function handleToggleCat() {
+  catSwitch.addEventListener("click", () => {
+    catSwitchBall.classList.toggle("switch-ball-toggled");
+  
+    const isChecked = catSwitch.getAttribute('data-checked') === 'true';
+    catSwitch.setAttribute('data-checked', String(!isChecked));
+
+    if (isChecked) {
+      catPic.src = "pimpolho.jpeg";
+    } else {
+      catPic.src = "casca-de-bala.jpeg";
+    }
+  })
+}
+
